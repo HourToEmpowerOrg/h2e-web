f7e17385a95636a6b061e1e01786db04c39c9290
William <william.bertrand94@gmail.com>
2020-09-11 18:04:09 -0700

    teams send message on tutor signup

--- backend/h2e_api/main/dispatch/dispatcher.py
+++ backend/h2e_api/main/dispatch/dispatcher.py
@@ -1,4 +1,5 @@
 from h2e_api.main.services.email_service import send_email
+from h2e_api.main.services.msft_teams import TeamsService
 
 from h2e_api.main.dispatch.events import (
     TUTOR_SIGNUP,
@@ -10,14 +11,9 @@ admin_emails = ['w.bertrand@hey.com']
 
 class Dispatcher:
 
-    # Get application info, create email, send to admins
     @classmethod
-    def _handle_tutor_signup(cls, application):
+    def _send_email(cls, application):
         subject = 'New Tutor Application'
-
-        if not application:
-            return
-
         message = f'<h4>Tutor Application submitted at {application.created_at}</h4>'
         message += f'<p>-- Info --</p>'
         message += f'<p>Name: {application.name}</p>'
@@ -26,6 +22,16 @@ class Dispatcher:
 
         for to_addr in admin_emails:
             send_email(subject, message, to_addr)
+
+    @classmethod
+    def _send_teams_message(cls, application):
+        ts = TeamsService()
+        ts.send_new_signup_message('tutor', application)
+
+    # Get application info, create email, send to admins
+    @classmethod
+    def _handle_tutor_signup(cls, application):
+        cls._send_teams_message(application)
 
     @classmethod
     def handle_event(cls, event, data):
