10ca221a47d45256782148cb7e99e1c297c5fc98
William <william.bertrand94@gmail.com>
2021-03-21 13:11:33 -0600

    fix requirements

--- backend/h2e_api/static/service-worker.js
+++ backend/h2e_api/static/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5a296cb4b512235fabd86a42c6e32db3.js"
+  "./precache-manifest.35a0dc16c6f9a60870720ca1160eeba6.js"
 );
 
 self.addEventListener('message', (event) => {
