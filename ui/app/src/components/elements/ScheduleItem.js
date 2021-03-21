a2cbb8d1bdbaa8f092d02dd1016850db75c6d4f4
William <william.bertrand94@gmail.com>
2021-03-21 12:20:27 -0600

    fix for day on schedule item

--- ui/app/src/components/elements/ScheduleItem.js
+++ ui/app/src/components/elements/ScheduleItem.js
@@ -7,19 +7,19 @@ var moment = require('moment');
 function dayFrom(dayNum) {
     switch(dayNum) {
         case 0:
-            return 'Sunday'
-        case 1:
             return 'Monday'
-        case 2:
+        case 1:
             return 'Tuesday'
-        case 3:
+        case 2:
             return 'Wednesday'
-        case 4:
+        case 3:
             return 'Thursday'
-        case 5:
+        case 4:
             return 'Friday'
-        case 6:
+        case 5:
             return 'Saturday'
+        case 6:
+            return 'Sunday'
         default:
             return 'Day'
     }
